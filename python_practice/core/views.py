dc21898f5964f2109fbff7e11313c1c1d220edf6
gunjan-cerebulb <gunjan.singh@cerebulb.com>
2025-06-27 15:12:49 +0530

    Executed ORM queries

---
+++ python_practice/core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .forms import RestaurantForm
+
+
+def index(request):
+    if request.method == 'POST':
+        form = RestaurantForm(request.POST or None)
+        if form.is_valid():
+            print(form.cleaned_data)
+        else:
+            return render(request, 'core/index.html', {'form': form})
+    context={'form': RestaurantForm()}
+    return render(request, 'core/index.html', context)
