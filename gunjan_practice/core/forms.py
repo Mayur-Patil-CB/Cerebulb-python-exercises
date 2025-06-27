40797ce0f7caae755607cf8701d8275f6bb3cdb0
gunjan-cerebulb <gunjan.singh@cerebulb.com>
2025-06-27 15:41:31 +0530

    Renamed parent directory

---
+++ gunjan_practice/core/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
+from core.models import Restaurant
+
+
+class RestaurantForm(forms.ModelForm):
+    class Meta:
+        model = Restaurant
+        fields = ('name',)
